6d8efb710af12c7a9d07089777498968920d7e39
fuadmahmud <fuadmahmud10@gmail.com>
2025-04-21 01:29:45 +0700

    feat: initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>The page you looking for is unavailable, please check again your url or try again later</h2>
+    </div>
+  )
+}
